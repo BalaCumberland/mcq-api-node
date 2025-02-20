b1ea094f46747e83919ade2475c85ade8e9c336a
BalaCumberland <37269795+BalaCumberland@users.noreply.github.com>
2025-02-20 17:08:12 -0500

    "rmeoved deployement from git"

--- deployment/src/utils/response.js
+++
@@ -1,9 +0,0 @@
-exports.success = (data) => ({
-    statusCode: 200,
-    body: JSON.stringify(data),
-});
-
-exports.error = (message, code = 500) => ({
-    statusCode: code,
-    body: JSON.stringify({ error: message }),
-});
